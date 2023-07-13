5ce47db72b9bff1819fa70d7a83ae754b5bd1849
m-janssens-boop <127706155+m-janssens-boop@users.noreply.github.com>
2023-07-13 14:55:37 -0700

    working on gauge chart

--- visualizations/viz_js/viz_logic.js
+++ visualizations/viz_js/viz_logic.js
@@ -1,159 +1,301 @@
 //Copied structure from previous assignment, found here https://github.com/m-janssens-boop/belly_button_challenge.git
 //placeholder code that will be changed later to fit actual project needs
 
-function buildStockOne(tickerOne) {
+// function buildStockOne(tickerOne) {
 
-  function buildMetadata(tickerOne) {
+//   function buildMetadata(tickerOne) {
 
-    // Access the website and use d3 to operate on the data
-    // read in url
-    const url = "URL GOES HERE";
+//     // Access the website and use d3 to operate on the data
+//     // read in url
+//     const url = `https://x6s1zdiuy5.execute-api.us-west-2.amazonaws.com/default/stockdata?ticker=${tickerOne}`;
 
-    //fetch the JSON data and console log it
-   d3.json(url).then((data) => {
-    console.log(data);
+//     //fetch the JSON data and console log it
+//    d3.json(url).then((data) => {
+//     console.log(data);
 
-    // Filter the data for the object with the desired ticker
-    let metadata = data.metadata;
-    //THIS CALL MAY NEED TO CHANGE DEPENDING ON HOW THE DATA IS FORMATTED
-    let filteredArray = metadata.filter(tickerOneObj => tickerOneObj.ticker == tickerOne);
-    let result = filteredArray[0];
-    // Select the panel with ticker of `#stock-one-metadata`
-    let panel = d3.select("#stock-one-metadata");
+//     // Select the panel with ticker of `#stock-one-metadata`
+//     let panel = d3.select("#stock-one-metadata");
   
-    // Clear existing metadata - use `.html("")`
-    panel.html("");
-  
-    // Append new tags for each key-value in the metadata
-    for (key in result){
-      panel.append("h6").text(`${key.toUpperCase()}: ${result[key]}`)
-    };
-    });
-  };
-
-  function buildChart(tickerOne) {
-    // Access the website and use .then to operate on the data
-        // read in url
-    const url = "URL GOES HERE";
-
-    //fetch the JSON data and console log it
-    d3.json(url).then((data) => {
-      // Filter the data for the object with the desired ticker
-      let tickers = data.tickers;
-      let filteredTickersArray = tickers.filter(tickerOneObj => tickerOneObj.symbol == tickerOne);
-      let result = filteredTickersArray[0]
-      // Pull the desired information (ticker, long name, sector, daily volume, ) from your filtered data
-      let ticker = result.symbol;
-      let labels = result.otu_ids;
-      let values = result.sample_values;
-      
-      //BUILD LINE CHART WITH DAILY PRICE OVER TIME FOR THE LAST WEEK WITH BAR CHART OF VOLUME IMPOSED BEHIND?
-      // Build a Bubble Chart
-      let bubbleTrace = {
-       x: labels,
-       y: values,
-       mode: 'markers',
-       marker: {
-       color: labels,
-       size: values
-        }
-      };
-      let bubbleLayout = {
-      title: "Prevalence of OTUs",
-      xaxis: {
-        title: {
-          text: "OTU ID"
-        }
-      }
-      };
-      let bubbleData = [bubbleTrace]
-      Plotly.newPlot('stock-1', bubbleData, bubbleLayout);
-  
-    });
+//     // Clear existing metadata - use `.html("")`
+//     panel.html("");
     
-  };
+//     // grab latest data
+//     let result = data.date
+//     // Append new tags for each key-value in the metadata
+//     for (key in result){
+//       panel.append("h6").text(`${key.toUpperCase()}: ${result[key]}`)
+//     };
+//     });
+//   };
 
+//   function buildChart(tickerOne) {
+//     // Access the website and use .then to operate on the data
+//         // read in url
+//     const url = "URL GOES HERE";
 
-  //INPUT BUILD MAP FUNCTION
-  function buildMap() {
-
-  
-
-  }
-
-  //THIS PART IS UPDATED. NEEDS BOTH FUNCTIONS AT END TO BE UNCOMMENTED
-  function init() {
-    // Get the reference to the dropdown menu
-    let selector = d3.select("#selStockOne")
-  
-    // Use the list of tickers to populate the select options
-    const url = "stock_data_2023-07-11.json";
-
-    //fetch the JSON data and console log it
-    d3.json(url).then((data) => {
-      //CHANGE THIS
-    // Do this by pulling the array associated with `names` 
-      let dicts = data[0].ticker;
+//     //fetch the JSON data and console log it
+//     d3.json(url).then((data) => {
+//       // Filter the data for the object with the desired ticker
+//       let tickers = data.tickers;
+//       let filteredTickersArray = tickers.filter(tickerOneObj => tickerOneObj.symbol == tickerOne);
+//       let result = filteredTickersArray[0]
+//       // Pull the desired information (ticker, long name, sector, daily volume, ) from your filtered data
+//       let ticker = result.symbol;
+//       let labels = result.otu_ids;
+//       let values = result.sample_values;
       
-      // Loop through the names and append to the dropdown menu
-      for (let i = 0; i < dicts.length; i++){
-        selector.append("option").text(dicts[i]).property("value",dicts[i]);
-      };
+//       //BUILD gauge chart for 52 week metrics
+//       // Trig to calc meter point
+//   let degrees = 180 - level;
+//   let radius = 0.5;
+//   let radians = (degrees * Math.PI) / 180;
+//   let x = radius * Math.cos(radians);
+//   let y = radius * Math.sin(radians);
+
+//   // Path: may have to change to create a better triangle
+//   let mainPath = "M -.0 -0.05 L .0 0.05 L ";
+//   let pathX = String(x);
+//   let space = " ";
+//   let pathY = String(y);
+//   let pathEnd = " Z";
+//   let path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+//   let data = [
+//     {
+//       type: "scatter",
+//       x: [0],
+//       y: [0],
+//       marker: { size: 12, color: "850000" },
+//       showlegend: false,
+//       name: "Freq",
+//       text: level,
+//       hoverinfo: "text+name"
+//     },
+//     {
+//       values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+//       rotation: 90,
+//       text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+//       textinfo: "text",
+//       textposition: "inside",
+//       marker: {
+//         colors: [
+//           "rgba(0, 105, 11, .5)",
+//           "rgba(10, 120, 22, .5)",
+//           "rgba(14, 127, 0, .5)",
+//           "rgba(110, 154, 22, .5)",
+//           "rgba(170, 202, 42, .5)",
+//           "rgba(202, 209, 95, .5)",
+//           "rgba(210, 206, 145, .5)",
+//           "rgba(232, 226, 202, .5)",
+//           "rgba(240, 230, 215, .5)",
+//           "rgba(255, 255, 255, 0)"
+//         ]
+//       },
+//       labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+//       hoverinfo: "label",
+//       hole: 0.5,
+//       type: "pie",
+//       showlegend: false
+//     }
+//   ];
+
+//   let layout = {
+//     shapes: [
+//       {
+//         type: "path",
+//         path: path,
+//         fillcolor: "850000",
+//         line: {
+//           color: "850000"
+//         }
+//       }
+//     ],
+//     title: "<b>52 Week High/Low</b> <br> And Latest Close",
+//     height: 500,
+//     width: 500,
+//     xaxis: {
+//       zeroline: false,
+//       showticklabels: false,
+//       showgrid: false,
+//       range: [-1, 1]
+//     },
+//     yaxis: {
+//       zeroline: false,
+//       showticklabels: false,
+//       showgrid: false,
+//       range: [-1, 1]
+//     }
+//   };
+
+//   let GAUGE = document.getElementById("gauge");
+//   Plotly.newPlot(GAUGE, data, layout);
+
   
-      // Use the first sample from the list to build the initial plots
-      // let firstTicker = tickers[0]
-      // buildChart(firstTicker)
-      // buildMetadata(firstTicker)
-    })
-    ;
+//     });
+    
+//   };
+
+
+//   //INPUT BUILD MAP FUNCTION
+//   function buildMap() {
+
+  
+
+//   }
+
+//   //THIS PART IS UPDATED. NEEDS BOTH FUNCTIONS AT END TO BE UNCOMMENTED
+//   function init() {
+//     // Get the reference to the dropdown menu
+//     let selector = d3.select("#selStockOne")
+  
+//       // Use the list of tickers to populate the select options
+//       const url = "https://znjanxz3h6.execute-api.us-west-2.amazonaws.com/default";
+  
+//       //fetch the JSON data and console log it
+//       d3.json(url).then((data) => {
+//         let tickers = data.body
+//         // Loop through the names and append to the dropdown menu
+//         for (let i = 0; i < tickers.length; i++){
+//           let ticker = tickers[i][0];
+//           console.log(ticker)
+//           selector.append("option").text(ticker).property("value",ticker);
+//         };
+  
+//       // Use the first sample from the list to build the initial plots
+//       // let firstTicker = tickers[0]
+//       // buildChart(firstTicker)
+//       // buildMetadata(firstTicker)
+//     })
+//     ;
+//   }
+
+//   function optionChanged(newTicker) {
+//     // Change your data and update your plots/metadata when newTicker is selected from the dropdown
+//     buildChart(newTicker);
+//     buildMetadata(newTicker);
+  
+//   };
+
+//   // Initialize the dashboard
+//     init();
+
+// }
+
+// function buildStockTwo(tickerTwo) {
+  
+
+// //COPY buildStockOne WHEN COMPLETE AND UPDATE WITH tickerTwo THROUGHOUT
+
+
+// }
+
+
+
+
+function buildChart() {
+  // Access the website and use .then to operate on the data
+      // read in url
+  const url = "./stock_data_2023-07-11.json"; 
+
+  //fetch the JSON data and console log it
+  d3.json(url).then((data) => {
+    
+    // Filter the data for the object with the desired ticker
+    let info = data[0]
+    // Pull the  52 week low/high and the latest close
+    let low = info["52WeekLow"];
+    let high = info["52WeekHigh"];
+    let close = info["previousClose"]
+    
+    //BUILD gauge chart for 52 week metrics
+    // Trig to calc meter point
+let degrees = 180 - close;
+let radius = 0.5;
+let radians = (degrees * Math.PI) / 180;
+let x = radius * Math.cos(radians);
+let y = radius * Math.sin(radians);
+
+// Path: may have to change to create a better triangle
+let mainPath = "M -.0 -0.05 L .0 0.05 L ";
+let pathX = String(x);
+let space = " ";
+let pathY = String(y);
+let pathEnd = " Z";
+let path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+let plotData = [
+  {
+    type: "scatter",
+    x: [0],
+    y: [0],
+    marker: { size: 12, color: "850000" },
+    showlegend: false,
+    name: "Freq",
+    text: level,
+    hoverinfo: "text+name"
+  },
+  {
+    values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+    rotation: 90,
+    text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+    textinfo: "text",
+    textposition: "inside",
+    marker: {
+      colors: [
+        "rgba(0, 105, 11, .5)",
+        "rgba(10, 120, 22, .5)",
+        "rgba(14, 127, 0, .5)",
+        "rgba(110, 154, 22, .5)",
+        "rgba(170, 202, 42, .5)",
+        "rgba(202, 209, 95, .5)",
+        "rgba(210, 206, 145, .5)",
+        "rgba(232, 226, 202, .5)",
+        "rgba(240, 230, 215, .5)",
+        "rgba(255, 255, 255, 0)"
+      ]
+    },
+    labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+    hoverinfo: "label",
+    hole: 0.5,
+    type: "pie",
+    showlegend: false
   }
+];
 
-  function optionChanged(newTicker) {
-    // Change your data and update your plots/metadata when newTicker is selected from the dropdown
-    buildChart(newTicker);
-    buildMetadata(newTicker);
-  
-  };
-
-  // Initialize the dashboard
-    init();
-
-}
-
-function buildStockTwo(tickerTwo) {
-  
-
-//COPY buildStockOne WHEN COMPLETE AND UPDATE WITH tickerTwo THROUGHOUT
-
-
-}
-
-
-
-
-
-
-function init() {
-  // Get the reference to the dropdown menu
-  let selector = d3.select("#selStockOne")
-
-    // Use the list of tickers to populate the select options
-    const url = "https://znjanxz3h6.execute-api.us-west-2.amazonaws.com/default";
-
-    //fetch the JSON data and console log it
-    d3.json(url).then((data) => {
-      let tickers = data.body
-      // Loop through the names and append to the dropdown menu
-      for (let i = 0; i < tickers.length; i++){
-        let ticker = tickers[i][0];
-        console.log(ticker)
-        selector.append("option").text(ticker).property("value",ticker);
-        // <option value="AAPL">AAPL</option>
-      };
+let layout = {
+  shapes: [
+    {
+      type: "path",
+      path: path,
+      fillcolor: "850000",
+      line: {
+        color: "850000"
+      }
+    }
+  ],
+  title: "<b>Latest Close</b> <br>Bounded By 52 Week High/Low ",
+  height: 500,
+  width: 500,
+  xaxis: {
+    zeroline: false,
+    showticklabels: false,
+    showgrid: false,
+    range: [-1, 1]
+  },
+  yaxis: {
+    zeroline: false,
+    showticklabels: false,
+    showgrid: false,
+    range: [-1, 1]
   }
+};
+
+let GAUGE = document.getElementById("gauge");
+Plotly.newPlot(GAUGE, plotData, layout);
+
+
+   });
   
-  )
-}
+};
 
-init();
-
+buildChart()
