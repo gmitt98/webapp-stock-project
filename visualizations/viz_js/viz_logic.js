5d9ff7704b721c66fc8a0cf5a7eed5c8ebcfb2b4
m-janssens-boop <127706155+m-janssens-boop@users.noreply.github.com>
2023-07-13 19:48:20 -0700

    added flex conatiner for gauge graphs below other graphs, made working gauge graph

--- visualizations/viz_js/viz_logic.js
+++ visualizations/viz_js/viz_logic.js
@@ -208,90 +208,29 @@ function buildChart() {
     let close = info["previousClose"]
     
     //BUILD gauge chart for 52 week metrics
-    // Trig to calc meter point
-let degrees = 180 - close;
-let radius = 0.5;
-let radians = (degrees * Math.PI) / 180;
-let x = radius * Math.cos(radians);
-let y = radius * Math.sin(radians);
-
-// Path: may have to change to create a better triangle
-let mainPath = "M -.0 -0.05 L .0 0.05 L ";
-let pathX = String(x);
-let space = " ";
-let pathY = String(y);
-let pathEnd = " Z";
-let path = mainPath.concat(pathX, space, pathY, pathEnd);
-
-let plotData = [
-  {
-    type: "scatter",
-    x: [0],
-    y: [0],
-    marker: { size: 12, color: "850000" },
-    showlegend: false,
-    name: "Freq",
-    text: level,
-    hoverinfo: "text+name"
-  },
-  {
-    values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
-    rotation: 90,
-    text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
-    textinfo: "text",
-    textposition: "inside",
-    marker: {
-      colors: [
-        "rgba(0, 105, 11, .5)",
-        "rgba(10, 120, 22, .5)",
-        "rgba(14, 127, 0, .5)",
-        "rgba(110, 154, 22, .5)",
-        "rgba(170, 202, 42, .5)",
-        "rgba(202, 209, 95, .5)",
-        "rgba(210, 206, 145, .5)",
-        "rgba(232, 226, 202, .5)",
-        "rgba(240, 230, 215, .5)",
-        "rgba(255, 255, 255, 0)"
-      ]
-    },
-    labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
-    hoverinfo: "label",
-    hole: 0.5,
-    type: "pie",
-    showlegend: false
-  }
-];
-
-let layout = {
-  shapes: [
+    //semi-circle gauge chart
+   let plotData = [
     {
-      type: "path",
-      path: path,
-      fillcolor: "850000",
-      line: {
-        color: "850000"
-      }
+      domain: {x: [0,1], y: [0,1]},
+      value: close,
+      title: {text: "Previous Close in Relation to 52 Week High/Low"},
+      type: "indicator",
+      mode: "gauge+number+delta",
+      delta: { reference: low },
+    gauge: {
+      axis: { range: [low, high] },
+      // steps: [
+      //   { range: [0, low], color: "lightgray" },
+      //   { range: [250, 400], color: "gray" }
+      // ],
     }
-  ],
-  title: "<b>Latest Close</b> <br>Bounded By 52 Week High/Low ",
-  height: 500,
-  width: 500,
-  xaxis: {
-    zeroline: false,
-    showticklabels: false,
-    showgrid: false,
-    range: [-1, 1]
-  },
-  yaxis: {
-    zeroline: false,
-    showticklabels: false,
-    showgrid: false,
-    range: [-1, 1]
-  }
-};
+    }
+   ];
 
-let GAUGE = document.getElementById("gauge");
-Plotly.newPlot(GAUGE, plotData, layout);
+   let layout = {width: 600, height: 500, margin: { t: 0, b: 0 } };
+
+    let GAUGE = document.getElementById("gauge-1");
+    Plotly.newPlot(GAUGE, plotData, layout);
 
 
    });
